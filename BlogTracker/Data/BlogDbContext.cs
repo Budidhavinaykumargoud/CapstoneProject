4700063e89c52f8c1edea82fd8d296f2d45c117e
Budidhavinaykumargoud <vinaygoud911@gmail.com>
2024-01-24 12:55:44 +0530

    Capstoneproject

---
+++ BlogTracker/Data/BlogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BlogTracker.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogTracker.Models.BlogInfo> BlogInfo { get; set; } = default!;
+
+        public DbSet<BlogTracker.Models.EmpInfo>? EmpInfo { get; set; }
+        public DbSet<BlogTracker.Models.AdminInfo> AdminInfo { get; set; }
+    }
+}
